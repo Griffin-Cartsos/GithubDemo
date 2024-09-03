dba14f0ccffbd38310ba34e4cc00a0dc60dc39f4
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-03 14:55:48 -0400

    Update and rename h.java to hello.java

--- h.java
+++
@@ -1,5 +0,0 @@
-public class h {
-    public static void main(String[]args) {
-            if (11 > 1) System.out.println("Hello!");
-    }
-}
---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[]args) {
+        if (11 > 1) {
+                System.out.println("Hello!")
+        };
+    }
+}
