078f81e498cd31ceb1fe8fdc88049700d088853a
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-03 14:50:18 -0400

    Update h.java

--- h.java
+++ h.java
@@ -1,8 +1,5 @@
 public class h {
-
     public static void main(String[]args) {
-
-        if (11 > 1) System.out.println("Hello!");
-
+            if (11 > 1) System.out.println("Hello!");
     }
 }
