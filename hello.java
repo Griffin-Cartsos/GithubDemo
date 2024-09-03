276aa79b017f6c01815667d1f7d661974a2fbe15
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-03 12:18:10 -0400

    Delete hello.java

--- hello.java
+++
@@ -1,8 +0,0 @@
-public class testClass {  // Class names should start with an uppercase letter
-    int badName;          // Variable names should start with a lowercase letter
-    static final int number=123; // 'number' should be in uppercase as it's a constant
-
-    void DoSomething() {  // Method names should start with a lowercase letter
-        int localVar = 1;
-    }
-}
