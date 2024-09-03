584f7209ce6a5d29dff88d0edc4a153f92504d67
Griffin-Cartsos <griffin.cartsos@gmail.com>
2024-09-03 12:07:35 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class testClass {  // Class names should start with an uppercase letter
-    int BADname;          // Variable names should start with a lowercase letter
+    int badName;          // Variable names should start with a lowercase letter
     static final int number=123; // 'number' should be in uppercase as it's a constant
 
     void DoSomething() {  // Method names should start with a lowercase letter
